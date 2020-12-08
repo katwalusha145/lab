3a328a88e517e75afa6340219203dad0ab432af1
“[usha <katwalusha145@gmail.com>
2020-12-08 13:09:26 +0545

    first commit

---
+++ app/src/main/java/com/usha/lab1/manager.kt
@@ -0,0 +1,11 @@
+package com.usha.lab1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class manager : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_manager)
+    }
+}
